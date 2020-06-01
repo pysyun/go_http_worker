0cf0d7b1a122f9d4c7c7edf4f5dc8ba3a5b58853
ChoNakyun <52971183+ChoNakyun@users.noreply.github.com>
2020-06-01 19:36:34 +0300

    Update main.go
    Updated HTTP program.
    Now we can use only TOR for Internet access.

--- main.go
+++ main.go
@@ -1,41 +1,54 @@
 package main
 
 import (
-	"net/http"
-	"log"
-	"io/ioutil"
-	"time"
-	"flag"
+        "net/http"
+        "log"
+        "io/ioutil"
+        "time"
+        "flag"
+        "os"
+        "net/url"
+        "golang.org/x/net/proxy"
 )
 
 var INTERVAL int
 var TARGET string
+var SOCKS5_PROXY_URI string
+
 
 func main() {
-	flag.IntVar(&INTERVAL, "INTERVAL", 1, "text description")
-	flag.StringVar(&TARGET, "TARGET", "http://google.com", "text description")
-	flag.Parse()
-	doEvery((time.Duration(INTERVAL) * time.Second), MakeRequest)
+        flag.IntVar(&INTERVAL, "INTERVAL", 1, "text description")
+        flag.StringVar(&TARGET, "TARGET", "https://check.torproject.org", "text description")
+        flag.StringVar(&SOCKS5_PROXY_URI, "SOCKS5_PROXY_URI", "127.0.0.1:9050", "text description")
+        flag.Parse()
+        doEvery((time.Duration(INTERVAL) * time.Second), MakeRequest)
 }
 
 func doEvery(d time.Duration, callback func()) {
-	for range time.Tick(d) {
-		callback()
-	}
+        for range time.Tick(d) {
+                callback()
+        }
 }
 
 func MakeRequest() {
-	resp, err := http.Get(TARGET)
-	if err != nil {
-		log.Fatalln(err)
-	}
 
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	
-	log.Println(string(body))
+        proxyURL, err := url.Parse("socks5://"+SOCKS5_PROXY_URI)
+        dialer, err := proxy.FromURL(proxyURL, proxy.Direct)
+        if err != nil {
+                        panic(err)
+        }
+        transport := &http.Transport{Dial: dialer.Dial}
+        client := http.Client{Transport: transport}
+
+
+        resp, err := client.Get(TARGET)
+        if err != nil {
+                log.Fatalln(err)
+        }
+
+        body, err := ioutil.ReadAll(resp.Body)
+        if err != nil {
+                log.Fatalln(err)
+        }
+        log.Println(string(body))
 }
-
-// go run /your/path/GET-Requests.go --INTERVAL 4 --TARGET "https://httpbin.org/get"
